456e29ba5abe513083cadf77b72ee407aae907d4
Guerrero Carrera Diego <diegoguerrero9361@gmail.com>
2025-03-06 10:48:52 -0600

    Practica 4

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -4,9 +4,10 @@
 int main() {
     int n;
     printf("Ingrese el tamaño del arreglo: ");
-    
+    scanf("%d", &n);
 
-    int *arr;
+    
+    int *arr=(int*)malloc(n*sizeof(int));
     if (arr == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         return 1;
@@ -14,15 +15,18 @@ int main() {
 
     printf("Ingrese %d valores:\n", n);
     for (int i = 0; i < n; i++) {
-        
+        scanf("%d\n", &arr[i]);
     }
 
     printf("El arreglo es: ");
     for (int i = 0; i < n; i++) {
+        printf("\nElemento[%d]: %d\n",i, arr[i]);
         
     }
     printf("\n");
-
     
+    printf("Liberando el espacio reservado\n");
+    free(arr);
+    arr=NULL;
     return 0;
 }
